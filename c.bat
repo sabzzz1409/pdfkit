9eb072fd2a38d4ff9c00c10ce1cad435478acc60
sabari s <ssgreeshan@gmail.com>
2025-05-16 09:55:02 +0530

    finale

--- c.bat
+++ c.bat
@@ -1,1 +1,10 @@
-code .
+@echo off
+if exist node_modules (
+  echo node_modules folder exists.
+  code .
+)
+else (
+  echo node_modules folder does not exist.
+  npm i --legacy-peer-deps
+  code .
+)
--- knitting.js
+++ knitting.js
@@ -3,6 +3,7 @@ import { createWriteStream } from 'fs';
 
 export const createPDF = () => {
 
+    // A4 configuration 
     // pagesize 595.28 x 841.89
     // page used 540 x 810
     // margin used 27.64 x 15.945
@@ -22,18 +23,18 @@ export const createPDF = () => {
 
     //pdf started
 
-    // text:string,x:number,y:number,options:object
+    // text --> text:string,x:number,y:number,options:object
     doc.fontSize(20).text('Invoice Title', 0, 25, { align: 'center', underline: 'black' })
-    
-    //draw box x:number,y:number,width:number,height:number
+
+    // box --> draw box x:number,y:number,width:number,height:number
     doc.rect(m_x, m_y, width, height).stroke('black')
-    
-    // draw lne x1,y1 to x2,y2
+
+    // line --> draw lne x1,y1 to x2,y2
     doc.moveTo(50, 100)   // starting point (x1, y1)
-   .lineTo(300, 150)  // ending point (x2, y2)
-   .stroke(); 
-    
-   // insert image source,x,y,option:{}
+        .lineTo(300, 150)  // ending point (x2, y2)
+        .stroke();
+
+    // image --> insert image source,x,y,option:{}
     doc.image('logo.png', 50, 150, { width: 200 });
 
 
